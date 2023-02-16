8cffc5e13d1984323125ef72b982aa5a54dfdc88
volodymyr-dehtiarenko <volodymyr.dehtiarenko@gmail.com>
2023-02-16 22:05:05 +0100

    release v1.0.1

--- flaskshop/settings_copy.py
+++ flaskshop/settings_copy.py
@@ -5,33 +5,27 @@ from pathlib import Path
 
 
 class DBConfig:
-    db_type = os.getenv("DB_TYPE", "postgresql")
-    user = os.getenv("DB_USER", "admin")
-    passwd = os.getenv("DB_PASSWD", "flatron")
+    db_type = os.getenv("DB_TYPE", "mysql")
+    user = os.getenv("DB_USER", "root")
+    passwd = os.getenv("DB_PASSWD", "123456")
     host = os.getenv("DB_HOST", "127.0.0.1")
-    port = os.getenv("DB_PORT", 5432)
-    db_name = os.getenv("DB_NAME", "db_24shop")
+    port = os.getenv("DB_PORT", 3306)
+    db_name = os.getenv("DB_NAME", "flaskshop")
     if db_type == "postgresql":
         db_uri = f"postgresql://{user}:{passwd}@{host}:{port}/{db_name}"
     elif db_type == "mysql":
         db_uri = (
             f"mysql+pymysql://{user}:{passwd}@{host}:{port}/{db_name}?charset=utf8mb4"
         )
-    redis_uri = "redis://localhost:8080"
+    redis_uri = "redis://localhost:6379"
     esearch_uri = "localhost"
 
 
 class Config:
-    
-    #DEBUG SETTING
-    DEBUG = True
-    TESTING = True
-    FLASKS_RUN_HOST = '194.182.78.151'
-    FLASK_RUN_PORT = '8080'
-
+    ENV = "dev"
+    FLASK_DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY", "thisisashop")
-    
-    #REDIS SETTING
+
     # Redis
     # if redis is enabled, it can be used for:
     #   - cache
@@ -48,13 +42,13 @@ class Config:
         os.getenv("ESEARCH_URI", DBConfig.esearch_uri),
     ]
 
-    # SQLALCHEMY SETTING
+    # SQLALCHEMY
     SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI", DBConfig.db_uri)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DATABASE_QUERY_TIMEOUT = 0.1  # log the slow database query, and unit is second
     SQLALCHEMY_RECORD_QUERIES = True
 
-    # DIR SETTING
+    # Dir
     APP_DIR = Path(__file__).parent  # This directory
     PROJECT_ROOT = APP_DIR.parent
     STATIC_DIR = APP_DIR / "static"
@@ -71,17 +65,15 @@ class Config:
     DEBUG_TB_PROFILER_ENABLED = True
 
     MESSAGE_QUOTA = 10
-    
-    # LANGUAGES SETTING
+
     LANGUAGES = {"en": "English", "bg": "Български"}
     BABEL_DEFAULT_LOCALE = os.getenv("BABEL_DEFAULT_LOCALE", "en_US")
     BABEL_DEFAULT_TIMEZONE = os.getenv("BABEL_DEFAULT_TIMEZONE", "UTC")
     BABEL_TRANSLATION_DIRECTORIES = os.getenv(
         "BABEL_TRANSLATION_DIRECTORIES", "../translations"
     )
-    BABEL_CURRENCY = os.getenv("BABEL_CURRENCY", "EUR")
-    
-    # MAIL SETTING
+    BABEL_CURRENCY = os.getenv("BABEL_CURRENCY", "USD")
+
     MAIL_SERVER = os.getenv("MAIL_SERVER", "localhost")
     MAIL_PORT = os.getenv("MAIL_PORT", 25)
     MAIL_TLS = os.getenv("MAIL_TLS", False)
@@ -91,8 +83,7 @@ class Config:
     GA_MEASUREMENT_ID = os.getenv("GA_MEASUREMENT_ID", "")
 
 
-#class ProdConfig(Config):
-    #ENV = "prod"
-    #ENV = "dev"
-    #FLASK_DEBUG = True
-    #DEBUG_TB_ENABLED = False
+class ProdConfig(Config):
+    ENV = "prod"
+    FLASK_DEBUG = False
+    DEBUG_TB_ENABLED = False
